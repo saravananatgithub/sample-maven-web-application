8a53752ac6bf6e2b6b2be78eb6c93b76ea2ea810
saravananatgithub <38750533+saravananatgithub@users.noreply.github.com>
2022-10-15 12:06:43 +0530

    Update BasicTest.java

--- src/main/java/com/mt/services/BasicTest.java
+++ src/main/java/com/mt/services/BasicTest.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.Test
+import org.testng.annotations.Test;
 
 public class BasicTest {
 
